4dd009c5758293154b255e2ac36b1101ccb705d1
Vedansh Wani <me.vedanshwani@gmail.com>
2021-07-24 12:50:05 +0530

    added navbar

--- client/src/Routes/Routes.jsx
+++ client/src/Routes/Routes.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import { Home } from "../Pages/Home";
 
 export const Routes = () => {
   return (
     <div>
       <Switch>
-        <Route></Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
       </Switch>
     </div>
   );
